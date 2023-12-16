5cb2979bc7ecebd871ceb7437d37802b7b9d3a71
J-Skarbek <john@snarkmediaagency.com>
2023-12-16 13:33:21 -0600

    Finish a basic node app that mimics the prior exercise

--- server.js
+++ server.js
@@ -4,6 +4,29 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   console.log('Request Made');
   console.log(req.url, req.method);
+
+  res.setHeader('Content-Type', 'text/html');
+
+  let path = './views/';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      break;
+    case '/about':
+      path += 'about.html';
+      break;
+    case '/contact':
+      path += 'contact.html';
+      break;
+    default:
+      path += '404.html'
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    err ? res.end() : res.end(data);
+  })
 })
 
 server.listen(8080, 'localhost', () => {
